0ec57734057a7c25ea3e65f6a38a9ef9e40a369f
Bautista-Baiocchi-lora <bautistabaiocchilora@gmail.com>
2021-03-24 18:01:38 -0300

    SyntaxToken parsing complete

---
+++ syntax/src/main/java/edu/austral/ingsis/SyntaxTable.java
@@ -0,0 +1,64 @@
+package edu.austral.ingsis;
+
+import edu.austral.ingsis.tokens.SyntaxToken;
+import edu.austral.ingsis.tokens.Token;
+import edu.austral.ingsis.tokens.TokenType;
+
+import java.util.HashMap;
+
+public class SyntaxTable {
+
+    private final HashMap<String, TokenType> table = new HashMap<>();
+
+    public SyntaxTable(){
+        table.put("let", TokenType.LET);
+        table.put("number", TokenType.NUMBER_TYPE);
+        table.put(";", TokenType.SEMICOLON);
+        table.put(":", TokenType.COLON);
+        table.put("=", TokenType.EQUALS);
+        table.put("string", TokenType.STRING_TYPE);
+        table.put("(", TokenType.L_PARENTHESES);
+        table.put(")", TokenType.R_PARANTHESES);
+        table.put("+", TokenType.ADDITION_OP);
+        table.put("-", TokenType.SUBTRACTION_OP);
+        table.put("%", TokenType.DIVISION_OP);
+        table.put("*", TokenType.MULTIPLICATION_OP);
+        table.put("\"", TokenType.DOUBLE_QUOTATION);
+        table.put("'", TokenType.SINGLE_QUOTATION);
+        table.put("println", TokenType.PRINTLN);
+    }
+
+    public boolean contains(Token token){
+        return table.containsKey(token.getValue());
+    }
+
+    public SyntaxToken truncate(Token token, TokenType type){
+        table.put(token.getValue(), type);
+
+        return classify(token);
+    }
+
+    public SyntaxToken classify(Token token){
+        return new SyntaxToken() {
+            @Override
+            public TokenType getType() {
+                return table.get(token.getValue());
+            }
+
+            @Override
+            public String getValue() {
+                return token.getValue();
+            }
+
+            @Override
+            public Integer getLineNumber() {
+                return token.getLineNumber();
+            }
+
+            @Override
+            public Integer getLineIndex() {
+                return token.getLineIndex();
+            }
+        };
+    }
+}
---
+++ syntax/src/main/java/edu/austral/ingsis/tokens/Token.java
@@ -0,0 +1,7 @@
+package edu.austral.ingsis.tokens;
+
+public interface Token {
+    String getValue();
+    Integer getLineNumber();
+    Integer getLineIndex();
+}
