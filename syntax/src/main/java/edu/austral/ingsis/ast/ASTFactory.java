a9097a4e0780ea607c840de10efae10ea2d604f0
Bautista-Baiocchi-lora <bautistabaiocchilora@gmail.com>
2021-03-26 15:10:09 -0300

    needs work

--- syntax/src/main/java/edu/austral/ingsis/ast/ASTFactory.java
+++ syntax/src/main/java/edu/austral/ingsis/ast/ASTFactory.java
@@ -1,9 +1,14 @@
 package edu.austral.ingsis.ast;
 
 import edu.austral.ingsis.SyntaxTable;
+import edu.austral.ingsis.ast.nodes.AssignationNode;
 import edu.austral.ingsis.tokens.SyntaxToken;
+import edu.austral.ingsis.tokens.TokenType;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ASTFactory {
 
@@ -11,10 +16,21 @@ public class ASTFactory {
     public void create(List<SyntaxToken> tokens, SyntaxTable table){
         final NodeBlock block =  new NodeBlock();
 
+        int lineCount = tokens.get(tokens.size() - 1).getLineNumber();
+        final Map<Integer, List<SyntaxToken>> lines = tokens.stream().collect(Collectors.groupingBy(SyntaxToken::getLineNumber));
 
+        for(int line = 0;line < lineCount;line++){
+            final Node node = processLine(lines.get(line));
+            block.addNode(node);
+        }
     }
 
-    public Node crawl(){
+    public Node processLine(List<SyntaxToken> line){
+
+        return null;
+    }
+
+    public AssignationNode buildAssignationNode(List<SyntaxToken> line){
 
         return null;
     }
