27fd961eb2122a5855f21b7220725893278e0a18
EliasDL <elias.leonov@ing.austral.edu.ar>
2021-04-16 23:10:12 -0300

    wip

--- cli/src/main/java/austral/ing/sis/App.java
+++ cli/src/main/java/austral/ing/sis/App.java
@@ -18,18 +18,6 @@ public class App implements Callable<Integer> {
   private Lexer lexer = new Lexer();
 
   private void run() {
-//      FileReaderPS fileReaderPS =
-//
-//            List<String> document = austra.ingsis.FileReaderPS.read(filePath);
-//            val tokens = lexer.tokenize(document);
-//            val nodes = .parse(tokens);
-//
-//            // here validates
-//            if (validateOnly) ValidatorImpl.run(nodes);
-//            else InterpreterImpl.run(nodes);
-//        ValidatorImpl.run(nodes);
-//        if (validateOnly) return;
-//        InterpreterImpl.run(nodes);
   }
 
   @Override
